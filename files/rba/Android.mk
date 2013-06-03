3ad711d2ebd4f9051b830b279866e0ce685ddf6c
Paul Mathieu <paul.mathieu@supaero.org>
2013-06-03 23:52:23 +0200

    Forgot the important stuff

---
+++ files/rba/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+stlibs := boost_date_time boost_filesystem boost_program_options boost_regex
+stlibs += boost_system boost_thread console_bridge cpp_common
+stlibs += rosbag_storage roscpp_serialization rostime bz2
+
+shlibs := $(stlibs)
+
+define include_shlib
+$(eval include $$(CLEAR_VARS))
+$(eval LOCAL_MODULE := $(1))
+$(eval LOCAL_SRC_FILES := $$(LOCAL_PATH)/lib/lib$(1).so)
+$(eval include $$(PREBUILT_SHARED_LIBRARY))
+endef
+define include_stlib
+$(eval include $$(CLEAR_VARS))
+$(eval LOCAL_MODULE := $(1))
+$(eval LOCAL_SRC_FILES := $$(LOCAL_PATH)/lib/lib$(1).a)
+$(eval include $$(PREBUILT_STATIC_LIBRARY))
+endef
+
+#$(foreach shlib,$(shlibs),$(eval $(call include_shlib,$(shlib))))
+$(foreach stlib,$(stlibs),$(eval $(call include_stlib,$(stlib))))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := rosbag
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_CPPFLAGS := -fexceptions -frtti
+#LOCAL_SRC_FILES := dummy.cpp
+#LOCAL_EXPORT_LDLIBS := $(foreach l,$(shlibs),-l$(l)) -L$(LOCAL_PATH)/lib
+#LOCAL_EXPORT_LDLIBS := -lstdc++ #-L$(LOCAL_PATH)/lib
+#LOCAL_SHARED_LIBRARIES := $(shlibs)
+LOCAL_STATIC_LIBRARIES := $(stlibs) gnustl_static
+
+include $(BUILD_STATIC_LIBRARY)
