0c53066557e2ab4dc44c41b80ed6bde2e2e8d4ba
Chad Rockey <chadrockey@willowgarage.com>
2013-11-06 12:57:01 -0800

    Pcl from tarball

--- get_pcl.sh
+++ get_pcl.sh
@@ -11,6 +11,9 @@ if [ $# != 1 ] || [ $1 == '-h' ] || [ $1 == '--help' ]; then
 fi
  
 prefix=$(cd $1 && pwd)
-URL=https://github.com/patmarion/pcl-superbuild
+#URL=https://github.com/patmarion/pcl-superbuild
  
-[ -d $prefix/pcl ] || git clone $URL $prefix/pcl
+#[ -d $prefix/pcl ] || git clone $URL $prefix/pcl
+
+URL=https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.0.tar.gz
+download_gz $URL $prefix
