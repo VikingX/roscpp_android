08f0619ce1f66d3a1c3222a23460ca5ba6c3b3ad
Chad Rockey <chadrockey@willowgarage.com>
2013-11-06 20:19:31 -0800

    Working compile of navigation stack.

--- config.sh
+++ config.sh
@@ -2,3 +2,4 @@ system=$(uname -s | tr 'DL' 'dl')-$(uname -m)
 gcc_version=4.6
 toolchain=arm-linux-androideabi-$gcc_version
 platform=android-14
+PYTHONPATH=/opt/ros/hydro/lib/python2.7/dist-packages:$PYTHONPATH
